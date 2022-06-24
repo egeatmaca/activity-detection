38434da52bcef1f33122ebc4d88b6e3863d1b050
humanbeing <egeatmaca98@gmail.com>
2022-06-24 14:00:24 +0200

    aggregate prediction results

--- README.md
+++ README.md
@@ -19,14 +19,10 @@ This needs to be done because OpenCV packages are incompatiable with eachother, 
 
 ---
 
-## Run real-time prediction with docker using webcam
-#### build from Docker image:
+## Run real-time prediction with webcam and docker:
 1. Build:  <pre><code> docker build -t activity-detection . </code></pre>
 2. Add Root to Access Control List: <pre><code> xhost +local:root </code></pre>
 3. Run: <pre><code> docker run --pid=host -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix:ro --privileged --device=/dev/video0 activity-detection </code></pre> 
-#### using Docker Compose:
-1. Add Root to Access Control List: <pre><code> xhost +local:root </code></pre>
-2. Run: <pre><code> docker-compose up </code></pre>
 
 ---
 #### Resources
--- activity_detection_prediction.py
+++ activity_detection_prediction.py
@@ -3,6 +3,9 @@ import mediapipe as mp
 from feature_extraction import estimate_pose, calculate_all_angles
 from visualization import draw_landmarks, draw_text
 import pickle
+import requests
+import os
+import numpy as np
 
 mp_drawing = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
@@ -17,6 +20,9 @@ if __name__ == '__main__':
 
     # Setup mediapipe instance
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+        # Initialize probability list
+        probas, proba_counter = [], 0
+        activity = ''
         while cap.isOpened():
             ret, frame = cap.read()
 
@@ -34,15 +40,27 @@ if __name__ == '__main__':
             try:
                 # Calculate angles
                 features = calculate_all_angles(results.pose_landmarks.landmark)
-                # Predict the activity
-                y = activity_detector.predict(features)[0]
+                # Predict the activity proba and append to the list
+                y = activity_detector.predict_proba(features)[0][1]
+                probas.append(y)
+                proba_counter += 1
+                # Aggregate prediction results and decide activity for the time frame
+                if proba_counter == 50:
+                    mean_proba = np.mean(probas)
+                    if mean_proba >= 0.5:
+                        activity = 'Working'
+                        # requests.get('http://'+os.environ['LS_HOST']+'/events/trigger?eventID=1')
+                    else:
+                        activity = 'Not Working'
+                        # requests.get('http://'+os.environ['LS_HOST']+'/events/trigger?eventID=0')
+                    probas = []
+                    proba_counter = 0
+                    print('Aggregated Result:', activity)
+
                 # Render activity detection
-                if y > 0.5 :
-                    draw_text(image, 'Working')
-                    print('Working')
-                else:
-                    draw_text(image, 'Not Working')
-                    print('Not Working')
+                draw_text(image, activity)
+                print('Working Proba:', y)
+
             except Exception as e:
                 print(str(e))
 
